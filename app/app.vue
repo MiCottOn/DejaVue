847bd5af7b4be03437bae6de1a780a854a5e0f18
Scott Schwartz <scottmschwrtz@gmail.com>
2017-01-21 15:25:15 -0800

    tree visualization!!! still some edge case alg fixes to be done need tree to show on devtool not inspectedwindow

--- app/app.vue
+++ app/app.vue
@@ -42,7 +42,8 @@ export default {
       inspectDOM: function() {  
         let domNodes;
         chrome.devtools.inspectedWindow.eval(
-          `          domNodes = inspect($$('body'));
+          `                    
+          domNodes = inspect($$('body'));
           
           function createDV() {
 
@@ -89,74 +90,180 @@ export default {
               if(child.$children.length > 0) findComponents(child)
             });
 
-            };
+          };
 
-            for (let i = 0; i < rootNodes.length; i += 1) {
-              findComponents(rootNodes[i])
-            }   
+          for (let i = 0; i < rootNodes.length; i += 1) {
+            findComponents(rootNodes[i])
+          }   
 
-            if (components.includes(rootNodes[0].__vue__)) console.log('rooooot')            
-            if (!components.includes(rootNodes[0].__vue__)) components.unshift(rootNodes[0].__vue__); 
+          if (components.includes(rootNodes[0].__vue__)) console.log('rooooot')            
+          if (!components.includes(rootNodes[0].__vue__)) components.unshift(rootNodes[0].__vue__); 
 
-            function CompConstructor(node) {
-          // -> _uid
-              this.id = node._uid;
-          // $vnode -> .tag -> replace(/vue-component-\d-/g, '')
-              this.name = node.$vnode.tag.replace(/vue-component-\d-/g, '');
-          // $parent -> _uid
-              this.parent = node.$parent._uid;
-          //
-              this.children = [];
-          // grab _data object - get keys array - filter keys - forEach on new array to add props to this object
-              this.variables = [];
-              this.props = [];
-              this.slots = [];
-          // this.directives = [];
+          function CompConstructor(node) {
+        // -> _uid
+            this.id = node._uid;
+        // $vnode -> .tag -> replace(/vue-component-\d-/g, '')
+            if(node.$options._componentTag === undefined) this.name = "root"
+            else this.name = node.$options._componentTag;
+        // $parent -> _uid
+            this.parentID = node.$parent._uid;
+            if(node.$parent.$vnode === undefined) this.parentName = undefined;
+            else if (node.$parent.$options._componentTag !== undefined) this.parentName = node.$parent.$options._componentTag;
+            else this.parentName = "root";
+        //
+            this.children = [];
+        // grab _data object - get keys array - filter keys - forEach on new array to add props to this object
+            this.variables = [];
+            this.props = [];
+            this.slots = [];
+        // this.directives = [];
+          }
+
+          // const fullComponents = [];
+          // console.log('root', rootNodes)
+
+
+
+          console.log('vue components', components)
+          const dvComponents = [];
+
+          function createDvComps(components) {
+
+            for (let i = 0; i < components.length; i += 1) {
+
+            node = components[i];
+
+            dvComponents.push(new CompConstructor(node));
+
+            const varKeys = Object.keys(node.$data).filter((key) => {
+              if (key.match(/\s/g)) return false;
+              return true;
+            });
+
+            if (varKeys) {
+              varKeys.forEach((variable) => {
+                if (variable) dvComponents[dvComponents.length - 1].variables.push({ [variable]: node.$data[variable] });
+              });
             }
 
-            // const fullComponents = [];
-            // console.log('root', rootNodes)
+            if(node.$slots.default) 
+              {
+                dvComponents[dvComponents.length - 1].slots.push(node.$slots.default[0].text);
+            }    
 
+        }
+            return dvComponents;
+          };
 
+          createDvComps(components);
+          console.log('deja vue components1',dvComponents)
+          
+          let data = []
 
-            console.log('vue components', components)
-            const dvComponents = [];
+          function treeNode(node) {
+            this.name = node.name;
+            this.parent = node.parentName;
+          }
 
-            function createDvComps(components) {
+          dvComponents.forEach(function(node) {
+            data.push(new treeNode(node))
+          })
+          
+          console.log('data', data)
+                
 
-              for (let i = 0; i < components.length; i += 1) {
+          // create a name: node map
+          var dataMap = data.reduce(function(map, node) {
+              map[node.name] = node;
+              return map;
+          }, {});
 
-              node = components[i];
+          // create the tree array
+          var treeData = [];
+          data.forEach(function(node) {
+              // add to parent
+              var parent = dataMap[node.parent];
+              if (parent) {
+                  // create child array if it doesn't exist
+                  (parent.children || (parent.children = []))
+                      // add node to child array
+                      .push(node);
+              } else {
+                  // parent is null or missing
+                  treeData.push(node);
+              }
+          });
 
-              dvComponents.push(new CompConstructor(node));
+          treeData = Object.assign({}, treeData)[0];
 
-              const varKeys = Object.keys(node.$data).filter((key) => {
-                if (key.match(/\s/g)) return false;
-                return true;
+          console.log(treeData)
+          var margin = {top: 20, right: 90, bottom: 30, left: 90},
+            width = 660 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+          // declares a tree layout and assigns the size
+          var treemap = d3.tree()
+            .size([height, width]);
+          // show what we've got
+          // Set the dimensions and margins of the diagram
+          //  assigns the data to a hierarchy using parent-child relationships
+            var nodes = d3.hierarchy(treeData, function(d) {
+              return d.children;
               });
 
-              if (varKeys) {
-                varKeys.forEach((variable) => {
-                  if (variable) dvComponents[dvComponents.length - 1].variables.push({ [variable]: node.$data[variable] });
+            // maps the node data to the tree layout
+            nodes = treemap(nodes);
+
+            // append the svg object to the body of the page
+            // appends a 'group' element to 'svg'
+            // moves the 'group' element to the top left margin
+            var svg = d3.select("body").append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom),
+              g = svg.append("g")
+                .attr("transform",
+                  "translate(" + margin.left + "," + margin.top + ")");
+
+            // adds the links between the nodes
+            var link = g.selectAll(".link")
+              .data( nodes.descendants().slice(1))
+              .enter().append("path")
+              .attr("class", "link")
+              .attr("d", function(d) {
+                return "M" + d.y + "," + d.x
+                + "C" + (d.y + d.parent.y) / 2 + "," + d.x
+                + " " + (d.y + d.parent.y) / 2 + "," + d.parent.x
+                + " " + d.parent.y + "," + d.parent.x;
                 });
-              }
 
-              if(node.$slots.default) 
-                {
-                  dvComponents[dvComponents.length - 1].slots.push(node.$slots.default[0].text);
-              }    
+            // adds each node as a group
+            var node = g.selectAll(".node")
+              .data(nodes.descendants())
+              .enter().append("g")
+              .attr("class", function(d) { 
+                return "node" + 
+                (d.children ? " node--internal" : " node--leaf"); })
+              .attr("transform", function(d) { 
+                return "translate(" + d.y + "," + d.x + ")"; });
 
+            // adds the circle to the node
+            node.append("circle")
+              .attr("r", 10);
+
+            // adds the text to the node
+            node.append("text")
+              .attr("dy", ".35em")
+              .attr("x", function(d) { return d.children ? -13 : 13; })
+              .style("text-anchor", function(d) { 
+              return d.children ? "end" : "start"; })
+              .text(function(d) { return d.data.name; });
+
+          ;
           }
-              return dvComponents;
-            };
+    
+          createDV()
 
-            createDvComps(components);
-            console.log('deja vue components',dvComponents)
-            return dvComponents
-
-      }
-        
-        createDV()
+          
           `
         )
         
