c6979a9eb467e351a9b991901a3c7bd1601fdec9
Scott Schwartz <scottmschwrtz@gmail.com>
2017-01-25 20:31:08 -0800

    visualization updates on inspected page change

--- app/assets/js/content-script.js
+++ app/assets/js/content-script.js
@@ -1,16 +1,4 @@
-let modificationList = []
-        MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-        var observer = new MutationObserver(function(mutations, observer) {
-            for(var mutation in mutations){
-                modificationList.push(mutation);
-                chrome.extension.sendMessage({'reRender': true})
-                console.log(modificationList)
-            }
-        });
-
-        observer.observe(document, {
-            subtree: true,
-            attributes: true,
-            characterData: true,
-            childList:true
-        });
+function setSelectedElement() {
+    console.log(document.querySelectorAll('*').length)
+    return document.querySelectorAll('*').length
+}
