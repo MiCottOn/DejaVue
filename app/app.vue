19c0373fd7971276dc88c148cabde8f728e17bdf
Scott Schwartz <scottmschwrtz@gmail.com>
2017-02-06 22:47:31 -0800

    start of stress testing using jquery to record events using local storage to save

--- app/app.vue
+++ app/app.vue
@@ -6,8 +6,9 @@
       </div>
       <div id="headerRight">
         <ul>
-          <li v-bind:class="{active: a}" @click="toggleActive"><router-link to="/tree">Component Tree</router-link></li>
-          <li v-bind:class="{active: b}" @click="toggleActive"><router-link to="/render">Render Tests</router-link></li>
+          <li v-bind:class="{active: a}" @click="toggleActive"><router-link to="/tree">Application Visualization</router-link></li>
+          <!--<li v-bind:class="{active: c}" @click="toggleActive"><router-link to="/timeline">Time Traveler</router-link></li>-->
+          <li v-bind:class="{active: b}" @click="toggleActive"><router-link to="/render">Stress Testing</router-link></li>
         </ul>
       </div>
     </header>    
@@ -23,19 +24,20 @@
 <script>  
 import Home from './home.vue';
 import Tree from './tree.vue';
-import Render from './render.vue';
+import Testing from './testing.vue';
 // import { findProps } from './parseLogic/findProps';
 // ^ we should do something like this for best practice
 
 export default {
-  name: 'app',
-  components: { Tree, Render },
+  name: 'DejaVue',
+  components: { Tree, Testing },
   data () {
     return {
       title: 'DejaVue',
       tagline: 'Vue component visualizer',
       a: true,
-      b: false
+      b: false,
+      c: false
     }
   }, 
   methods: {
@@ -43,10 +45,18 @@ export default {
       if (this.a) {
         this.a = false;
         this.b = true;
+        this.c = false;
       }
-      else {
+      else if (this.b) {
         this.b = false;
         this.a = true;
+        this.c = false;
+      }
+
+      else {
+        this.b = false;
+        this.a = false;
+        this.c = true;
       }
     }
   }
---
+++ app/timeline.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="treeContainer">
+    <div id="tabSwitcher">
+      <div id="timelinePage">Page</div>      
+      <div id="timelineTree">Tree</div>      
+    </div>
+
+    <iframe width="100%" height="700px" id="timelineFrame">
+
+
+    </iframe>
+  </div>
+
+
+  
+</template>
+
+<script>
+ 
+export default {
+  name: 'timeline',
+
+  data () {
+    return {
+      title: 'Timeline'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
