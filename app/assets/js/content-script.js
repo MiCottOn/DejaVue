efd78eb25ab5bb37869a2a1976adb4e11693d8e2
Scott Schwartz <scottmschwrtz@gmail.com>
2017-02-09 18:35:32 -0800

    fixed color issues on dev tool dark theme full width svg

--- app/assets/js/content-script.js
+++ app/assets/js/content-script.js
@@ -1,60 +1,60 @@
-//CAPTURE EVENTS ON PAGES - START OF SCRIPT/ALG
-let actions = []
-$(document).ready(function () {
-    $('a').on('click', function (e) {
-        actions.push({
-            'Click': [e, $(this)]
-        });
-        console.log(actions)
+//Stress testing start
+// let actions = []
+// $(document).ready(function () {
+//     $('a').on('click', function (e) {
+//         actions.push({
+//             'Click': [e, $(this)]
+//         });
+//         console.log(actions)
 
-        chrome.storage.local.set({
-            'macros': actions
-        }, function () {
-            return
-        });
+//         chrome.storage.local.set({
+//             'macros': actions
+//         }, function () {
+//             return
+//         });
 
-        chrome.storage.local.get('macros', function (result) {
-            if (result.macros) {
-                console.log('macros', result.macros);
-            }
-        });
-    })
-});
+//         chrome.storage.local.get('macros', function (result) {
+//             if (result.macros) {
+//                 console.log('macros', result.macros);
+//             }
+//         });
+//     })
+// });
 
+let count;
+let oldCount;
+let check;
 
+//inspect page for node count changes and if change then capture state on devtools.js
 
-        let count;
-        let oldCount;
-        let check;
-
-        nodeCounter = function () {
-            //FIX FOR TIME TRAVEL - MAKE SOME SORT OF CHECK SO NEW STATE ISN'T ADDED
-            chrome.storage.sync.get('traveledThroughTime', function (result) {
-                if (!result.traveledThroughTime) {
-                    count = document.querySelectorAll('*').length - document.querySelectorAll('div.highlighter').length;
-                    chrome.storage.sync.get('count', function () {
-                        check = count;
-                    });
-                    if (count !== check) {
-                        chrome.storage.sync.set({
-                            'count': count
-                        }, function () {
-                            return
-                        });
-                    }
-                }
-            })
-        }
-
-        firstNodeCounter = function () {
-            oldCount = document.querySelectorAll('*').length;
-            chrome.storage.sync.set({
-                'oldCount': count
-            }, function () {
-                return
+nodeCounter = function () {
+    //FIX FOR TIME TRAVEL - MAKE SOME SORT OF CHECK SO NEW STATE ISN'T ADDED
+    chrome.storage.sync.get('traveledThroughTime', function (result) {
+        if (!result.traveledThroughTime) {
+            count = document.querySelectorAll('*').length - document.querySelectorAll('div.highlighter').length;
+            chrome.storage.sync.get('count', function () {
+                check = count;
             });
+            if (count !== check) {
+                chrome.storage.sync.set({
+                    'count': count
+                }, function () {
+                    return
+                });
+            }
         }
+    })
+}
 
-        window.onload = firstNodeCounter();
+firstNodeCounter = function () {
+    oldCount = document.querySelectorAll('*').length;
+    chrome.storage.sync.set({
+        'oldCount': count
+    }, function () {
+        return
+    });
+}
 
-        setInterval(nodeCounter, 100)
+window.onload = firstNodeCounter();
+
+setInterval(nodeCounter, 100)
